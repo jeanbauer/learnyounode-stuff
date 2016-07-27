eaa9f5b56cc2df39341c9d55e7d4ac7dc7b55f24
jeanbauer <jeanbauerc@gmail.com>
2016-07-27 00:11:52 -0300

    adding 2 to 7 stream solutions

---
+++ stream-adventure/5-lines.js
@@ -0,0 +1,56 @@
+/* Instead of transforming every line as in the previous "TRANSFORM" example,
+for this challenge, convert even-numbered lines to upper-case and odd-numbered
+lines to lower-case. Consider the first line to be odd-numbered. For example
+given this input:
+
+    One
+    Two
+    Three
+    Four
+
+Your program should output:
+
+    one
+    TWO
+    three
+    FOUR
+
+You can use the `split` module to split input by newlines. For example:
+
+    var split = require('split');
+    process.stdin
+        .pipe(split())
+        .pipe(through2(function (line, _, next) {
+            console.dir(line.toString());
+            next();
+        }))
+    ;
+
+`split` will buffer chunks on newlines before you get them. In the previous
+example, we will get separate events for each line even though all the data
+probably arrives on the same chunk:
+
+    $ echo -e 'one\ntwo\nthree' | node split.js
+    'one'
+    'two'
+    'three'
+
+Your own program should use `split` in this way, but you should transform the
+input and pipe the output through to `process.stdout`.
+
+Make sure to `npm install split through2` in the directory where your solution
+file lives. */
+
+
+const through = require('through');
+const split = require('split');
+
+let lineNum = 0;
+const write = function (buffer) {
+    const strBuffer = buffer.toString();
+    this.queue(lineNum % 2 ? `${strBuffer.toUpperCase()}\n`: `${strBuffer.toLowerCase()}\n`);
+    lineNum++;
+};
+const tr = through(write);
+
+process.stdin.pipe(split()).pipe(tr).pipe(process.stdout);
